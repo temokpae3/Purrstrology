0082888ed8b86ab3748574900db23f2b9ff2fe48
TemitopeEmokpae <temokpae@oswego.edu>
2024-03-10 09:33:41 -0400

    updated bash

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Navigate to the base directory
-cd templates
+cd "$(dirname "$0")"
 
 # Install dependencies if needed
 # Example:
 # npm install
+
+# Navigate to the templates directory
+cd templates
 
 # Run your build command
 python run.py
