1000c56f58cf4983721d8feb2e6cbe1e6376bdff
TemitopeEmokpae <temokpae@oswego.edu>
2024-03-10 09:45:35 -0400

    updated run.py

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
+import os
 from init import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Set Flask environment to production
+    os.environ['FLASK_ENV'] = 'production'
+    # Run the Flask app
+    app.run()
